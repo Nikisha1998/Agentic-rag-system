c5aba0ab64c32eb03ee3f0794f686ab3cdfa4310
Nikisha1998 <nikishabongale12@gmail.com>
2025-07-19 20:52:30 +0530

    tested on same retrival, rag is working as needed

--- ask_agent.py
+++ ask_agent.py
@@ -1,8 +1,27 @@
 from langchain.chains import RetrievalQA
-from langchain.llms import HuggingFacePipeline
+# from langchain.llms import HuggingFacePipeline
+from langchain_community.llms import HuggingFacePipeline
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
 from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
+
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import RetrievalQA
+
+# Define a cleaner prompt template
+prompt_template = PromptTemplate.from_template(
+    """
+    You are a helpful medical assistant. Based on the following discharge summary, answer the question clearly and concisely.
+
+    Context:
+    {context}
+
+    Question: {question}
+
+    Answer in one sentence:
+    """
+)
 
 def ask_question(query, persist_dir="db"):
     model_name = "google/flan-t5-base"
@@ -16,5 +35,12 @@ def ask_question(query, persist_dir="db"):
     vectordb = Chroma(persist_directory=persist_dir, embedding_function=embeddings)
 
     retriever = vectordb.as_retriever()
-    qa = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
+    # qa = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
+    # Now create the RetrievalQA with custom prompt
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        retriever=retriever,
+        chain_type="stuff",
+        chain_type_kwargs={"prompt": prompt_template}
+    )
     return qa.run(query)
--- loader.py
+++ loader.py
@@ -1,7 +1,12 @@
-from langchain.document_loaders import PyMuPDFLoader
+# from langchain.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
+# from langchain.vectorstores import Chroma
+# from langchain.embeddings import HuggingFaceEmbeddings
+
+
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def load_and_index(pdf_path, persist_dir="db"):
     loader = PyMuPDFLoader(pdf_path)
@@ -11,6 +16,8 @@ def load_and_index(pdf_path, persist_dir="db"):
     split_docs = splitter.split_documents(docs)
 
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    vectordb = Chroma.from_documents(split_docs, embedding_function=embeddings, persist_directory=persist_dir)
-    vectordb.persist()
+    vectordb = Chroma.from_documents(documents=split_docs,
+                embedding=embeddings,
+                persist_directory=persist_dir)
+    # vectordb.persist()
     return vectordb
